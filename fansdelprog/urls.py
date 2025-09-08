9298db05e8698a663eb934589cc06f4b647f969d
Frappuccinoic <arseeicore@gmail.com>
2025-09-07 21:52:14 -0300

    Nombre de proyecto cambiado en todos los archivos

--- fansdelprog/urls.py
+++ fansdelprog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for miweb project.
+URL configuration for fansdelprog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
