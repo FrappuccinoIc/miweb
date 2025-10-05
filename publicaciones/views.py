b9453e1bd694bf8cd91e456cbce1b37d4996256f
Frappuccinoic <arseeicore@gmail.com>
2025-10-05 19:52:40 -0300

    Segundo intento paginacion

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage
 from .models import Publicacion
 
-def foro(req):
-    publicacion=Publicacion.objects.all()
+def foro(request):
+    publicacion=Publicacion.objects.all().order_by('created')
     p = Paginator(publicacion, 2)
-    num_pag = req.GET.get('page', 1)    # Segundo arg es default
+    num_pag = request.GET.get('page', 1)    # Segundo arg es default
 
     try: pag = p.page(num_pag)
-    except: pag = p.page(1)
+    except: 
+        pag = p.page(1)
+        print("paso error")
 
-    return render(req, "publicaciones/foro.html", {"paginacion": pag, "num_posts": len(publicacion)})
+    return render(request, "publicaciones/foro.html", {"paginacion": pag, "num_posts": len(publicacion)})
