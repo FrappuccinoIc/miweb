dceb7d8b8862cdca0f28e1ec4c5f0adbcf5207c6
Frappuccinoic <arseeicore@gmail.com>
2025-10-05 20:12:08 -0300

    Cambiado estilo de post

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -5,12 +5,12 @@ from django.core.paginator import Paginator
 
 def foro(req):
     publicacion=Publicacion.objects.all()
-    p = Paginator(publicacion, 2)
+    p = Paginator(publicacion, 1)
     num_pag = req.GET.get('page', 1)    # Segundo arg es default
 
     page_number = req.GET.get('page')
-    
-        page_obj = paginator.get_page(page_number)
-        print("paso error")
 
-    return render(request, "publicaciones/foro.html", {"page_obj": page_obj})
+    page_obj = p.get_page(page_number)
+    print("paso error")
+
+    return render(req, "publicaciones/foro.html", {"page_obj": page_obj})
