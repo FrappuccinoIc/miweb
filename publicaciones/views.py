0cb79350e66f055d2749bf88a5b22d4d43aa4545
Frappuccinoic <arseeicore@gmail.com>
2025-10-05 19:33:02 -0300

    Paginación a medias

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
 from .models import Publicacion
 
 def foro(req):
     publicacion=Publicacion.objects.all()
-    return render(req, "publicaciones/foro.html", {"publicacion": publicacion})
+    p = Paginator(publicacion, 2)
+    num_pag = req.GET.get('page', 1)    # Segundo arg es default
+
+    try: pag = p.page(num_pag)
+    except: pag = p.page(1)
+
+    return render(req, "publicaciones/foro.html", {"paginacion": pag, "num_posts": len(publicacion)})
