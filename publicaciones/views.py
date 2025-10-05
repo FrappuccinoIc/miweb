5a2eea8576536799b0efab2a9fbba235ddc559e3
Natzabes <sr786382@gmail.com>
2025-10-05 20:17:44 -0300

    reduccion de visualizacion de paginas en paginator

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage
 from .models import Publicacion
 from django.core.paginator import Paginator
 
 def foro(req):
-    publicacion=Publicacion.objects.all()
-    p = Paginator(publicacion, 1)
-    num_pag = req.GET.get('page', 1)    # Segundo arg es default
+    publicaciones = Publicacion.objects.all().order_by('id')
+    paginator = Paginator(publicaciones, 1)  # 1 artículo por página
 
     page_number = req.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    page_obj = p.get_page(page_number)
-    print("paso error")
+    # 🔹 Rango de páginas visibles (ejemplo: 5)
+    index = page_obj.number - 1  # índice actual
+    max_index = len(paginator.page_range)
+    start_index = max(index - 2, 0)
+    end_index = min(index + 3, max_index)
+    page_range = paginator.page_range[start_index:end_index]
 
-    return render(req, "publicaciones/foro.html", {"page_obj": page_obj})
+    return render(req, "publicaciones/foro.html", {
+        "page_obj": page_obj,
+        "page_range": page_range,  # 🔹 se pasa al template
+    })
